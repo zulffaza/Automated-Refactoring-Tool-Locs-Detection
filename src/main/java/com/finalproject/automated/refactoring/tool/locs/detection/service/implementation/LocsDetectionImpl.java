2f3cb176ff4c7fbb3c0dbfcca9e9d2f0e5964e46
zulffaza <zulffaza@gmail.com>
2019-03-04 15:43:32 +0700

    Change line counting method (remove 'for' for counted) but still stuck

--- src/main/java/com/finalproject/automated/refactoring/tool/locs/detection/service/implementation/LocsDetectionImpl.java
+++ src/main/java/com/finalproject/automated/refactoring/tool/locs/detection/service/implementation/LocsDetectionImpl.java
@@ -1,12 +1,16 @@
 package com.finalproject.automated.refactoring.tool.locs.detection.service.implementation;
 
 import com.finalproject.automated.refactoring.tool.locs.detection.service.LocsDetection;
+import com.finalproject.automated.refactoring.tool.locs.detection.service.model.CountingStatementVA;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author fazazulfikapp
@@ -22,17 +26,29 @@ public class LocsDetectionImpl implements LocsDetection {
     private static final String NEW_LINE_DELIMITER = "\n";
 
     private static final Character DOUBLE_QUOTE_CHARACTER = '\"';
+    private static final Character SINGLE_QUOTE_CHARACTER = '\'';
+    private static final Character OPEN_PARENTHESES_CHARACTER = '(';
+    private static final Character CLOSED_PARENTHESES_CHARACTER = ')';
     private static final Character ESCAPE_CHARACTER = '\\';
 
     private static final List<String> ESCAPES_KEYWORDS = Arrays.asList("//", "/*", "*/", "*", "import", "package");
 
     private static final List<Character> STATEMENTS_KEYWORDS = Arrays.asList(';', '{');
 
+    private static final Map<Character, Character> ESCAPES_STATEMENTS_KEYWORDS;
+
+    static {
+        ESCAPES_STATEMENTS_KEYWORDS = new HashMap<>();
+        ESCAPES_STATEMENTS_KEYWORDS.put(DOUBLE_QUOTE_CHARACTER, DOUBLE_QUOTE_CHARACTER);
+        ESCAPES_STATEMENTS_KEYWORDS.put(SINGLE_QUOTE_CHARACTER, SINGLE_QUOTE_CHARACTER);
+        ESCAPES_STATEMENTS_KEYWORDS.put(OPEN_PARENTHESES_CHARACTER, CLOSED_PARENTHESES_CHARACTER);
+    }
+
     @Override
     public Long llocDetection(@NonNull String body) {
         List<String> lines = Arrays.asList(body.split(NEW_LINE_DELIMITER));
 
-        return lines.parallelStream()
+        return lines.stream()
                 .map(String::trim)
                 .filter(this::isValid)
                 .mapToLong(this::countStatement)
@@ -52,36 +68,92 @@ public class LocsDetectionImpl implements LocsDetection {
     }
 
     private Long countStatement(String line) {
-        Stack<Character> stack = new Stack<>();
-        Boolean isEscape = Boolean.FALSE;
-        Long countedStatement = INITIAL_COUNT;
+        CountingStatementVA countingStatementVA = new CountingStatementVA();
+
+        System.out.println(line);
 
         for (int index = 0; index < line.length(); index++) {
-            Character character = line.charAt(index);
-
-            if (isEscape) {
-                isEscape = Boolean.FALSE;
-                continue;
-            }
-
-            if (character.equals(DOUBLE_QUOTE_CHARACTER)) {
-                if (stack.empty())
-                    stack.push(character);
-                else
-                    stack.pop();
-
-                continue;
-            }
-
-            if (character.equals(ESCAPE_CHARACTER)) {
-                isEscape = Boolean.TRUE;
-                continue;
-            }
-
-            if (stack.empty() && STATEMENTS_KEYWORDS.contains(character))
-                countedStatement++;
+            countingStatement(line.charAt(index), countingStatementVA);
         }
 
-        return countedStatement;
+        return countingStatementVA.getCountedStatement()
+                .get();
+    }
+
+
+    private void countingStatement(Character character, CountingStatementVA countingStatementVA) {
+        ifStringExpression(character, countingStatementVA);
+//        ifParenthesesExpression(character, countingStatementVA.getStack());
+        ifEscapeCharacter(character, countingStatementVA.getEscape());
+        ifStatement(character, countingStatementVA);
+    }
+
+    private void ifStringExpression(Character character, CountingStatementVA countingStatementVA) {
+        if (!countingStatementVA.getEscape().get()) {
+            Boolean isValid = (!countingStatementVA.getStack().empty() &&
+                    (countingStatementVA.getStack().peek().equals(DOUBLE_QUOTE_CHARACTER) ||
+                            countingStatementVA.getStack().peek().equals(SINGLE_QUOTE_CHARACTER)))
+                    || countingStatementVA.getStack().empty();
+
+            System.out.println("For : " + character + " is " + isValid);
+
+            if (isValid) {
+                if (ESCAPES_STATEMENTS_KEYWORDS.containsValue(character) && !countingStatementVA.getStack().empty()) {
+                    System.out.println(character);
+                    countingStatementVA.getStack().pop();
+                } else if (ESCAPES_STATEMENTS_KEYWORDS.containsKey(character)) {
+                    System.out.println(character);
+                    countingStatementVA.getStack().push(character);
+                }
+            }
+        }
+    }
+
+    //    ("(hello {\"name\"}), (good {\"greeting\"}), (morning {\"vibes\"})")
+
+    private Boolean isStringExpression(Character character, AtomicBoolean escape) {
+        return character.equals(DOUBLE_QUOTE_CHARACTER) &&
+                !escape.get();
+    }
+
+    private void ifParenthesesExpression(Character character, Stack<Character> stack) {
+        if (character.equals(OPEN_PARENTHESES_CHARACTER)) {
+            System.out.println(character);
+            stack.push(character);
+        }
+
+        if (isClosedParentheses(character, stack)) {
+            System.out.println(character);
+            stack.pop();
+        }
+    }
+
+    private Boolean isClosedParentheses(Character character, Stack<Character> stack) {
+        return !stack.empty() &&
+                character.equals(CLOSED_PARENTHESES_CHARACTER);
+    }
+
+    private void ifEscapeCharacter(Character character, AtomicBoolean escape) {
+        if (escape.get()) {
+            escape.set(Boolean.FALSE);
+        }
+
+        if (character.equals(ESCAPE_CHARACTER) && !escape.get()) {
+            escape.set(Boolean.TRUE);
+        }
+    }
+
+    private void ifStatement(Character character, CountingStatementVA countingStatementVA) {
+        if (isStatement(character, countingStatementVA.getStack())) {
+            System.out.println("Count...");
+            countingStatementVA.getCountedStatement().getAndIncrement();
+        } else if (!countingStatementVA.getStack().empty()) {
+            System.out.println("Peek --> " + countingStatementVA.getStack().peek());
+        }
+    }
+
+    private Boolean isStatement(Character character, Stack stack) {
+        return stack.empty() &&
+                STATEMENTS_KEYWORDS.contains(character);
     }
 }
--- src/test/java/com/finalproject/automated/refactoring/tool/locs/detection/service/implementation/LocsDetectionImplTest.java
+++ src/test/java/com/finalproject/automated/refactoring/tool/locs/detection/service/implementation/LocsDetectionImplTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 
 public class LocsDetectionImplTest {
 
-    private static final Long LLOC_COUNT = 15L;
+    private static final Long LLOC_COUNT = 48L;
 
     private LocsDetectionImpl locsDetection;
 
@@ -43,41 +43,125 @@ public class LocsDetectionImplTest {
     }
 
     private String createBody() {
-        return "package path;\n" +
+        return "package com.finalproject.automated.refactoring.tool.locs.detection.service.implementation;\n" +
                 "\n" +
-                "import java.io.Serializable;\n" +
+                "import com.finalproject.automated.refactoring.tool.locs.detection.service.LocsDetection;\n" +
+                "import com.finalproject.automated.refactoring.tool.locs.detection.service.model.CountingStatementVA;\n" +
+                "import lombok.NonNull;\n" +
+                "import org.springframework.stereotype.Service;\n" +
                 "\n" +
-                "public class Filename implements Serializable {\n" +
+                "import java.util.Arrays;\n" +
+                "import java.util.List;\n" +
+                "import java.util.Stack;\n" +
+                "import java.util.concurrent.atomic.AtomicBoolean;\n" +
                 "\n" +
-                "    private String name;\n" +
-                "    private String extension;\n" +
+                "/**\n" +
+                " * @author fazazulfikapp\n" +
+                " * @version 1.0.0\n" +
+                " * @since 24 October 2018\n" +
+                " */\n" +
                 "\n" +
-                "    @SuppressWarnings()\n" +
-                "    public Filename() throws Exception, IOException {\n" +
+                "@Service\n" +
+                "public class LocsDetectionImpl implements LocsDetection {\n" +
                 "\n" +
+                "    private static final Long INITIAL_COUNT = 0L;\n" +
+                "\n" +
+                "    private static final String NEW_LINE_DELIMITER = \"\\n\";\n" +
+                "\n" +
+                "    private static final Character DOUBLE_QUOTE_CHARACTER = '\\\"';\n" +
+                "    private static final Character OPEN_PARENTHESES_CHARACTER = '(';\n" +
+                "    private static final Character CLOSED_PARENTHESES_CHARACTER = ')';\n" +
+                "    private static final Character ESCAPE_CHARACTER = '\\\\';\n" +
+                "\n" +
+                "    private static final List<String> ESCAPES_KEYWORDS = Arrays.asList(\"//\", \"/*\", \"*/\", \"*\", \"import\", \"package\");\n" +
+                "\n" +
+                "    private static final List<Character> STATEMENTS_KEYWORDS = Arrays.asList(';', '{');\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public Long llocDetection(@NonNull String body) {\n" +
+                "        List<String> lines = Arrays.asList(body.split(NEW_LINE_DELIMITER));\n" +
+                "\n" +
+                "        return lines.parallelStream()\n" +
+                "                .map(String::trim)\n" +
+                "                .filter(this::isValid)\n" +
+                "                .mapToLong(this::countStatement)\n" +
+                "                .sum();\n" +
                 "    }\n" +
                 "\n" +
-                "    @SuppressWarnings()\n" +
-                "    public Filename(@NonNull String name, @NonNull String extension) throws Exception, IOException {\n" +
-                "        this.name = name;\n" +
-                "        this.extension = extension;\n" +
+                "    private Boolean isValid(String line) {\n" +
+                "        return !line.isEmpty() && !isEscapes(line);\n" +
                 "    }\n" +
                 "\n" +
-                "    public String getName() {\n" +
-                "        return name;\n" +
+                "    private Boolean isEscapes(String line) {\n" +
+                "        Long count = ESCAPES_KEYWORDS.stream()\n" +
+                "                .filter(line::startsWith)\n" +
+                "                .count();\n" +
+                "\n" +
+                "        return count > INITIAL_COUNT;\n" +
                 "    }\n" +
                 "\n" +
-                "    public void setName(@NonNull String name) {\n" +
-                "        this.name = name;\n" +
+                "    private Long countStatement(String line) {\n" +
+                "        CountingStatementVA countingStatementVA = new CountingStatementVA();\n" +
+                "\n" +
+                "        for (int index = 0; index < line.length(); index++) {\n" +
+                "            countingStatement(line.charAt(index), countingStatementVA);\n" +
+                "        }\n" +
+                "\n" +
+                "        return countingStatementVA.getCountedStatement()\n" +
+                "                .get();\n" +
                 "    }\n" +
                 "\n" +
-                "    public String getExtension() {\n" +
-                "        return extension;\n" +
+                "\n" +
+                "    private void countingStatement(Character character, CountingStatementVA countingStatementVA) {\n" +
+                "        ifStringExpression(character, countingStatementVA);\n" +
+                "        ifParenthesesExpression(character, countingStatementVA.getStack());\n" +
+                "        ifEscapeCharacter(character, countingStatementVA.getEscape());\n" +
+                "        ifStatement(character, countingStatementVA);\n" +
                 "    }\n" +
                 "\n" +
-                "    public void setExtension(@NonNull String extension) {\n" +
-                "        this.extension = extension;\n" +
+                "    private void ifStringExpression(Character character, CountingStatementVA countingStatementVA) {\n" +
+                "        if (isStringExpression(character, countingStatementVA.getEscape())) {\n" +
+                "            if (countingStatementVA.getStack().empty())\n" +
+                "                countingStatementVA.getStack().push(character);\n" +
+                "            else\n" +
+                "                countingStatementVA.getStack().pop();\n" +
+                "        }\n" +
                 "    }\n" +
-                "}";
+                "\n" +
+                "    private Boolean isStringExpression(Character character, AtomicBoolean escape) {\n" +
+                "        return character.equals(DOUBLE_QUOTE_CHARACTER) &&\n" +
+                "                !escape.get();\n" +
+                "    }\n" +
+                "\n" +
+                "    private void ifParenthesesExpression(Character character, Stack<Character> stack) {\n" +
+                "        if (character.equals(OPEN_PARENTHESES_CHARACTER)) {\n" +
+                "            stack.push(character);\n" +
+                "        }\n" +
+                "\n" +
+                "        if (character.equals(CLOSED_PARENTHESES_CHARACTER)) {\n" +
+                "            stack.pop();\n" +
+                "        }\n" +
+                "    }\n" +
+                "\n" +
+                "    private void ifEscapeCharacter(Character character, AtomicBoolean escape) {\n" +
+                "        if (escape.get()) {\n" +
+                "            escape.set(Boolean.FALSE);\n" +
+                "        }\n" +
+                "\n" +
+                "        if (character.equals(ESCAPE_CHARACTER) && !escape.get()) {\n" +
+                "            escape.set(Boolean.TRUE);\n" +
+                "        }\n" +
+                "    }\n" +
+                "\n" +
+                "    private void ifStatement(Character character, CountingStatementVA countingStatementVA) {\n" +
+                "        if (isStatement(character, countingStatementVA.getStack()))\n" +
+                "            countingStatementVA.getCountedStatement().getAndIncrement();\n" +
+                "    }\n" +
+                "\n" +
+                "    private Boolean isStatement(Character character, Stack stack) {\n" +
+                "        return stack.empty() &&\n" +
+                "                STATEMENTS_KEYWORDS.contains(character);\n" +
+                "    }\n" +
+                "}\n";
     }
 }
